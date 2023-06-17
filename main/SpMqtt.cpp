7be7e78f6ccb2e2c078cf83ddbcb3960ac35365e
wangkc <wkclz@qq.com>
2023-06-17 20:27:02 +0800

    mqtt 代码集成

---
+++ main/SpMqtt.cpp
@@ -0,0 +1,71 @@
+#include "Arduino.h"
+#include <PubSubClient.h>
+#include "SpMqtt.h"
+
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+// 初始化 MQTT
+SpMqtt::SpMqtt() {
+  espClient.setCACert(ca_cert);
+  client = PubSubClient(espClient);
+  client.setKeepAlive(120);
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+}
+
+
+// 连接/重连 MQTT
+void SpMqtt::reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect(mqtt_client_id, mqtt_username, mqtt_password)) {
+      Serial.println("connected: 连接成功");
+      client.subscribe(mqtt_subscribe);
+      // 连接成功，推送消息
+      client.publish(mqtt_push, "reconnected: mqtt connect success!");
+    } else {
+      int state = client.state();
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.print(" try again in 5 seconds: ");
+      if (state == 1) {
+        Serial.println("拒绝连接, 服务器不支持该客户端请求的 MQTT 协议");
+      }
+      if (state == 2) {
+        Serial.println("拒绝连接, 客户端 ID 是正确的 UTF-8 字符串，但是不被服务器允许, 或者心跳时间间隔不满足平台要求");
+      }
+      if (state == 3) {
+        Serial.println("拒绝连接, 网络连接已经建立，但是 MQTT 服务不可用");
+      }
+      if (state == 4) {
+        Serial.println("拒绝连接, 在用户名或密码中的数据是错误格式的");
+      }
+      if (state == 5) {
+        Serial.println("拒绝连接, 客户端的连接未被授权");
+      }
+      delay(5000);
+    }
+  }
+}
+
+// 检查 MQTT 消息
+void SpMqtt::checkMsg() {
+  unsigned long currentMillis = millis();
+  if(currentMillis - previousMillis >= checkInterval) {
+    if (!client.connected()) {
+      reconnect();
+    }
+    client.loop();
+  }
+
+}
+
--- main/main.ino
+++ main/main.ino
@@ -1,11 +1,13 @@
 #include "SpBase.h"
 #include "SpWifi.h"
 #include "SpLed.h"
+#include "SpMqtt.h"
 
 SpBase base;
 SpWifi wifi;
 SpLed led1(15, 100, 100);
 SpLed led2(2, 1000, 1000);
+SpMqtt mqtt;
 
 void setup() {
   Serial.begin(115200);
@@ -22,4 +24,5 @@ void loop() {
   wifi.checkAndConnect();
   led1.update();
   led2.update();
+  mqtt.checkMsg();
 }
