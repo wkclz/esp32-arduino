446d72d08dc5e8c4f164b73c45785566fb0a65c4
wangkc <wkclz@qq.com>
2023-08-30 00:11:12 +0800

    速度调整，以及相伴变化触发

--- main/SpStepMotor.cpp
+++ main/SpStepMotor.cpp
@@ -13,6 +13,25 @@ SpStepMotor::SpStepMotor(int pina, int pinb, int pinc, int pind) {
 }
 
 
+SpStepMotor::setSpeed(int speed) {
+  if (speed < -10) {
+    speed = -10;
+  }
+  if (speed > 1000) {
+    speed = 100;
+  }
+  // 小于 0 时为逆向
+  if (speed < 0) {
+    currentSpeed = 0 - speed;
+    currentDir = 0;
+  }
+  if (speed >= 0) {
+    currentSpeed = speed;
+    currentDir = 1;
+  }
+}
+
+
 /*******************************************************************************
 * 函 数 名       : step_motor_28BYJ48_send_pulse
 * 函数功能       : 输出一个数据给ULN2003从而实现向步进电机发送一个脉冲
@@ -20,17 +39,36 @@ SpStepMotor::SpStepMotor(int pina, int pinb, int pinc, int pind) {
                   dir：方向选择,1：顺时针,0：逆时针
 * 输    出       : 无
 *******************************************************************************/
-void SpStepMotor::sendPulse(unsigned char step,unsigned char dir) {
-  unsigned char temp = step;
+void SpStepMotor::sendPulse() {
 
-  //如果为逆时针旋转
-  if(dir == 0) {
-    // 调换节拍信号
-    temp = 7 - step;
+  unsigned long currentMillis = millis();
+  unsigned long checkInterval = 100 - currentSpeed;
+  if((currentMillis - previousMillis < checkInterval)) {
+    return;
+  }
+  previousMillis = currentMillis;
+  // 速度为0， 不转动
+  if (checkInterval >= maxSpeed) {
+    return;
+  }
+
+  // 正向
+  if (currentDir == 1) {
+    currentStep = currentStep + 1;
+    if (currentStep == 8) {
+      currentStep == 0;
+    }
+  }
+  // 逆向
+  if (currentDir == 0) {
+    currentStep = currentStep - 1;
+    if (currentStep == -1) {
+      currentStep == 7;
+    }
   }
 
   // 8个节拍控制：A->AB->B->BC->C->CD->D->DA
-  switch(temp) {
+  switch(currentStep) {
     case 0: digitalWrite(ina_pin,1);digitalWrite(inb_pin,0);digitalWrite(inc_pin,0);digitalWrite(ind_pin,0);break;
     case 1: digitalWrite(ina_pin,1);digitalWrite(inb_pin,1);digitalWrite(inc_pin,0);digitalWrite(ind_pin,0);break;
     case 2: digitalWrite(ina_pin,0);digitalWrite(inb_pin,1);digitalWrite(inc_pin,0);digitalWrite(ind_pin,0);break;
--- main/SpStepMotor.h
+++ main/SpStepMotor.h
@@ -9,6 +9,8 @@ class SpStepMotor {
     // 初始化电机
     SpStepMotor(int pina, int pinb, int pinc, int pind);
 
+    void setSpeed(int speed);
+
     /********************************************************************
     * 函 数 名       : sendPulse
     * 函数功能       : 输出一个数据给ULN2003从而实现向步进电机发送一个脉冲
@@ -16,7 +18,7 @@ class SpStepMotor {
                       dir：方向选择,1：顺时针,0：逆时针
     * 输    出       : 无
     ********************************************************************/
-    void sendPulse(unsigned char step,unsigned char dir);
+    void sendPulse();
 
   private:
 
@@ -26,12 +28,21 @@ class SpStepMotor {
     int inc_pin; //   0
     int ind_pin; //   4
 
-    // 定义步进电机速度，值越小，速度越快, 最小不能小于1
-    int maxSpeed = 1;
-    int minSpeed = 5;
+    // 定义步进电机速度，需要转换为时差，驱动时差才是速度
+    int maxSpeed = 100;
+    int minSpeed = -10;
+
+    // 当前 step
+    int currentStep = 0;
+
+    // 1 正向，0逆向
+    int currentDir = 1;
+
+    // 当前速度
+    int currentSpeed = 0;
 
     // 上一次状态变更的时间
-    // unsigned long previousMillis;
+    unsigned long previousMillis;
 
 };
 
